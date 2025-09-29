3d73997c4b21bd9d136725c5e2ddb033578fb55c
Johanabhishek <140403122+Johanabhishek@users.noreply.github.com>
2025-09-29 09:00:43 +0530

    Update Explore.tsx

--- src/pages/startup/Explore.tsx
+++ src/pages/startup/Explore.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { collection, getDocs, _DocumentData } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase-config';
 
 interface Startup {
